28fb9138b0d15bbce50ebd3b87b3444ff1c9b7a2
Timur Bolotov <bolotovtmr@gmail.com>
2023-07-22 00:55:11 +0600

    feat: edit link
    Signed-off-by: Timur Bolotov <bolotovtmr@gmail.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,65 +1,78 @@
-import { defineConfig, } from "vitepress";
+import { defineConfig, DefaultTheme } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
+
+const nav: DefaultTheme.NavItem[] = [
+  {
+    text: "OpenSearchCon",
+    items: [
+      {
+        text: "Register for OpenSearchCon!",
+        link: "https://opensearchcon2023.splashthat.com/",
+      },
+      { text: "OpenSearchCon 2023 CFP!", link: "/opensearchcon2023-cfp" },
+    ],
+  },
+  { text: "Downloads", link: "/downloads" },
+  {
+    text: "About",
+    items: [
+      { text: "Opensearch", link: "/about" },
+      { text: "Releases", link: "/releases" },
+      { text: "FAQ", link: "/faq" },
+    ],
+  },
+  {
+    text: "Community",
+    items: [
+      { text: "Blog", link: "/blog" },
+      { text: "Forum", link: "https://forum.opensearch.org/" },
+      { text: "Slack", link: "/slack" },
+      { text: "Events", link: "/events" },
+      { text: "Partners", link: "/partners" },
+    ],
+  },
+  { text: "Documentation", link: "/docs" },
+  {
+    text: "Platform",
+    items: [
+      { text: "Vector Database", link: "platform/search/vector-database" },
+      { text: "Live Demo", link: "https://playground.opensearch.org" },
+      { text: "Performance Benchmarks", link: "/benchmarks" },
+    ],
+  },
+];
+
 export default defineConfig({
   title: "OpenSearch Project",
-  outDir: 'dist',
+  outDir: "dist",
   lang: "en-US",
   description: "OpenSearch Project Website",
   themeConfig: {
-    logo: 'logo.svg',
-    search: {
-      provider: 'local',
-    },
-    nav: [
-      {
-        text: "OpenSearchCon",
-        items: [
-          {
-            text: "Register for OpenSearchCon!",
-            link: "https://opensearchcon2023.splashthat.com/",
-          },
-          { text: "OpenSearchCon 2023 CFP!", link: "/opensearchcon2023-cfp" },
-        ],
-      },
-      { text: "Downloads", link: "/downloads" },
-      {
-        text: "About",
-        items: [
-          { text: "Opensearch", link: "/about" },
-          { text: "Releases", link: "/releases" },
-          { text: "FAQ", link: "/faq" },
-        ],
-      },
-      {
-        text: "Community",
-        items: [
-          { text: "Blog", link: "/blog" },
-          { text: "Forum", link: "https://forum.opensearch.org/" },
-          { text: "Slack", link: "/slack" },
-          { text: "Events", link: "/events" },
-          { text: "Partners", link: "/partners" },
-        ],
-      },
-      { text: "Documentation", link: "/docs" },
-      {
-        text: "Platform",
-        items: [
-          { text: "Vector Database", link: "platform/search/vector-database" },
-          { text: "Live Demo", link: "https://playground.opensearch.org" },
-          { text: "Performance Benchmarks", link: "/benchmarks" },
-        ],
-      },
-    ],
 
+    logo: "logo.svg",
+    search: {
+      provider: "local",
+    },
+    nav,
+    editLink: {
+      pattern: 'https://github.com/timursaurus/opensearch-website/edit/main/docs/:path',
+      text: 'Suggest changes to this page',
+    },
     sidebar: [
       {
         text: "Getting Started",
         items: [
-          { text: 'Plugins', link: 'https://opensearch.org/docs/latest/install-and-configure/plugins/' },
-          { text: 'Docker Image', link: 'https://hub.docker.com/r/opensearchproject/opensearch' },
-          { text: 'Downloads', link: '/downloads' },
-        ]
+          {
+            text: "Plugins",
+            link: "https://opensearch.org/docs/latest/install-and-configure/plugins/",
+          },
+          {
+            text: "Docker Image",
+            link: "https://hub.docker.com/r/opensearchproject/opensearch",
+          },
+          { text: "Downloads", link: "/downloads" },
+        ],
       },
       {
         text: "Platform",
@@ -76,10 +89,13 @@ export default defineConfig({
 
     socialLinks: [
       { icon: "github", link: "https://github.com/opensearch-project" },
-      { icon: 'twitter', link: 'https://twitter.com/OpenSearchProj' },
-      { icon: 'youtube', link: 'https://www.youtube.com/c/OpenSearchProject' },
-      { icon: 'facebook', link: 'https://www.facebook.com/OpenSearchProject/' },
-      { icon: 'linkedin', link: 'https://www.linkedin.com/company/opensearch-project/' }
+      { icon: "twitter", link: "https://twitter.com/OpenSearchProj" },
+      { icon: "youtube", link: "https://www.youtube.com/c/OpenSearchProject" },
+      { icon: "facebook", link: "https://www.facebook.com/OpenSearchProject/" },
+      {
+        icon: "linkedin",
+        link: "https://www.linkedin.com/company/opensearch-project/",
+      },
     ],
   },
 });
